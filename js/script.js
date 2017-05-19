9bcb8f6d1e34552d2e0a42b10febd10941c3126f
banujan59 <banujan59@gmail.com>
2017-05-19 00:46:45 -0400

    More work on the nav bar
    Nav bar feature #2 added (see function navSlider's comments)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
+/* Global variables */
+	// The following are used for the nav bar creation
+	var brandOffset, aboutUSOffset, locationOffset, galleryOffset;
+/* Global variables */
+
+
 $(function() // to be executed when DOM finished loading
 {	
-	navSlider(); // implement the nav slider
+	// initialize global vars
+	initGlobalVars();
+	$(window).resize(initGlobalVars);
+	
+	// implement the nav slider
+	navSlider();
 });
 
+/**
+	* This function will initialize the global variables
+*/
+function initGlobalVars()
+{
+	// initialize global vars
+	brandOffset = Math.floor($("#brand").offset().top) - 50;
+	aboutUSOffset = Math.floor($("#aboutUs").offset().top) - 50;
+	locationOffset = Math.floor($("#location").offset().top) - 50;
+	galleryOffset = Math.floor($("#gallery").offset().top) - 50;
+}
 
 /**
-	* This function will modify the way the user navigates with the nav bar
-	* When a link is clicked, the page will smoothly go to the destination.
+	* This function will modify the way the user navigates with the nav bar. It has 2 main features:
+		1) When a link is clicked, the page will smoothly go to the destination.
+		2) Each link in the navigation bar will be change color according to the position of the nav bar
 */
 function navSlider()
 {
@@ -17,6 +40,7 @@ function navSlider()
 		setTimeout(function(){$("body").scrollTop(0);},20);
 	// END BUG FIX #1
 	
+	// Feature #1
 	$(".nav li a").click(function(e)
 	{
 		e.preventDefault(); // stops the browser's default behavior
@@ -25,9 +49,48 @@ function navSlider()
 		
 		if (anchorID.charAt(0) == '#' && anchorID.length > 1 && target.length > 0)
 		{
-			var speed = 1000; // speed of the animation (in miliseconds)
+			var time = 1000; // time of the animation (in miliseconds)
 			var pos = Math.max(target.offset().top, 0); // the target position
-			$("body").animate({ scrollTop: pos }, speed, 'swing');
+			$("body").animate({ scrollTop: pos }, time, 'swing');
 		}
+	});
+	
+	// Feature #2
+	$(window).scroll(function()
+	{
+		// variable that holds the current y-scroll value
+		var scrollPos = $("body").scrollTop();
+		
+		// if were are between section brand and about us
+		if(scrollPos >= brandOffset && scrollPos < aboutUSOffset)
+		{
+			$(".brandLink").css("color", "white");
+			$(".aboutUSLink, .locationLink, .galleryLink").css("color", "#9d9d9d");
+		}
+		
+		// if were are between section about us and location
+		else if(scrollPos >= aboutUSOffset && scrollPos < locationOffset)
+		{
+			$(".aboutUSLink").css("color", "white");
+			$(".brandLink, .locationLink, .galleryLink").css("color", "#9d9d9d");
+		}
+		
+		// if were are between section location and gallery
+		else if(scrollPos >= locationOffset && scrollPos < galleryOffset)
+		{
+			$(".locationLink").css("color", "white");
+			$(".aboutUSLink, .brandLink, .galleryLink").css("color", "#9d9d9d");
+		}
+		
+		// if were in section gallery
+		else if(scrollPos >= galleryOffset)
+		{
+			$(".galleryLink").css("color", "white");
+			$(".aboutUSLink, .locationLink, .brandLink").css("color", "#9d9d9d");
+		}
+		
+		// if we are at the top of the page
+		else
+			$(".aboutUSLink, .locationLink, .brandLink, .galleryLink").css("color", "#9d9d9d");
 	});
 }
